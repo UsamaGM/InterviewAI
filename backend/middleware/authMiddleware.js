0c782403fb12cdb45ca2f006bcd8d9749b7845a6
UsamaGM <usamapk7861@gmail.com>
2025-01-13 14:58:12 +0500

    configure mongodb and add middleware for security

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,19 @@
+const jwt = require("jsonwebtoken");
+
+const protect = (req, res, next) => {
+  const token = req.headers.authorization?.split(" ")[1]; // Bearer token
+
+  if (!token) {
+    return res.status(401).json({ message: "Not authorized!" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded.user; // Add user info to request
+    next();
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+module.exports = protect;
